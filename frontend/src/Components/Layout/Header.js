ab8a3ed9b5913098d9392087d949773d4f9e6e92
AlankritVerma07 <alankritverma.lko@gmail.com>
2022-02-25 00:32:40 -0800

    Added header with React

---
+++ frontend/src/Components/Layout/Header.js
@@ -0,0 +1,52 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import logo from "../../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <div className={classes.logo}>
+                <img src={logo} alt="Logo for the app" />
+                <a href="#">LOGO</a>
+              </div>
+            </li>
+            <li>
+              <div className={classes["movie-search"]}>
+                <input
+                  name="search"
+                  type="text"
+                  placeholder="Search by movie title"
+                  value=""
+                />
+                <i
+                  className={`fas fa-search ${classes["search-btn"]}`}
+                  aria-hidden="true"
+                ></i>
+              </div>
+            </li>
+            <li className={classes.fancy}>
+              <a href="#">Home</a>
+            </li>
+            <li className={classes.fancy}>
+              <a href="#">Movies</a>
+            </li>
+            <li className={classes.fancy}>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">
+                <button className={classes["btn-auth"]}>Sign up</button>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
