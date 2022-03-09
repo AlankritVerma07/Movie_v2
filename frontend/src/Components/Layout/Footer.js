0a7433feebb64baefaeec2c619582ad453112efe
AlankritVerma07 <alankritverma.lko@gmail.com>
2022-03-08 17:08:54 -0800

    Made footer sticky

--- frontend/src/Components/Layout/Footer.js
+++ frontend/src/Components/Layout/Footer.js
@@ -3,50 +3,47 @@ import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={classes.footer}>
-      <div className={classes["bottom-push"]}></div>
-      <footer>
-        <div className={classes["footer-logo"]}>
-          <img src={logo} alt="Logo for the app" />
-          <a href="#">LOGO</a>
-        </div>
-        <div className={classes.social}>
-          <h4>Stay Connected!</h4>
-          <ul>
-            <li>
-              <a href="#">
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-github"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-linkedin"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className={classes.message}>
-          &copy; Made with&nbsp;
-          <i className={`fas fa-heart ${classes.heart}`}></i> by&nbsp;
-          <a href="https://github.com/AlankritVerma07">Alan & Jackie</a>
-        </div>
-      </footer>
-    </div>
+    <footer>
+      <div className={classes["footer-logo"]}>
+        <img src={logo} alt="Logo for the app" />
+        <a href="#">LOGO</a>
+      </div>
+      <div className={classes.social}>
+        <h4>Stay Connected!</h4>
+        <ul>
+          <li>
+            <a href="#">
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={classes.message}>
+        &copy; Made with&nbsp;
+        <i className={`fas fa-heart ${classes.heart}`}></i> by&nbsp;
+        <a href="https://github.com/AlankritVerma07">Alan & Jackie</a>
+      </div>
+    </footer>
   );
 };
 export default Footer;
